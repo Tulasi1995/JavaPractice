36c37e43bc130121a8d7bc9362fd39fc38d99b37
Tulasi1995 <y12ame435@gmail.com>
2017-10-18 15:42:21 +0530

    Added a new Class

---
+++ StrutsValidation/src/com/sample/Testing.java
@@ -0,0 +1,9 @@
+package com.sample;
+
+public class Testing {
+	public static void main(String[] args) {
+		System.out.println("Hello I am testing github ");
+		System.out.println("sorry correction i am learning tha");
+	}
+
+}
