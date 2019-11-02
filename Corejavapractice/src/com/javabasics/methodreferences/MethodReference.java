c58676483c536cf730c91b3de7e70b17915cdf24
Tulasi1995 <y12ame435@gmail.com>
2019-11-02 13:57:39 +0530

    added static method references

--- Corejavapractice/src/com/javabasics/methodreferences/MethodReference.java
+++ Corejavapractice/src/com/javabasics/methodreferences/MethodReference.java
@@ -1,15 +1,45 @@
 package com.javabasics.methodreferences;
 
+
+/**
+ * 
+ * @author TulasiRam G
+ *
+ *This class gives examples about static method reference
+ */
 public class MethodReference {
 
+	public static void message() {
+		
+		System.out.println(" Greetings from static class");
+	}
+	
+	static int addition(int i,int j) {
+		
+		return i+j;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Greetings greet= MethodReference::message;
+		
+		greet.greet();
+		
+		MathCalu cal=MethodReference::addition;
+		
+		System.out.println(cal.opearation(20, 50));
 
 	}
 	
 	interface Greetings{
 		
 		 void greet();
+	}
+	
+	interface MathCalu{
+		
+		int opearation(int i,int j);
 	}
 
 }
