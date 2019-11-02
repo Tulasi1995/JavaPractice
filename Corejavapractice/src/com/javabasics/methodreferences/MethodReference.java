13033b5b1d0e299cc67c5ddbc16bbd1478c80b1c
Tulasi1995 <y12ame435@gmail.com>
2019-11-02 13:45:42 +0530

    method reference

---
+++ Corejavapractice/src/com/javabasics/methodreferences/MethodReference.java
@@ -0,0 +1,15 @@
+package com.javabasics.methodreferences;
+
+public class MethodReference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	interface Greetings{
+		
+		 void greet();
+	}
+
+}
